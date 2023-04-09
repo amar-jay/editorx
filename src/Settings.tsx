3e45c8eaa7f554e02188e978cf63ab98d921d490
amar-jay <64834413+amar-jay@users.noreply.github.com>
2023-04-09 03:38:56 +0300

    Update Settings.tsx
    convert from global css to module css 🚭

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { EditorState, options, useStore } from './store';
  import { shallow } from 'zustand/shallow';
 //import { EditorState } from './store';
-import './Settings.css';
+import styles from './Settings.css';
 
 // this is the settings component, it should hover over the editor and allow the user to change the settings
 // for the editor, such as the theme, font, etc.
@@ -11,25 +11,25 @@ import './Settings.css';
 export const Settings:React.FC<{toggleSettings: ()=>void}> = ({toggleSettings}) => {
     const state = useStore(state => state, shallow);
     return (
-        <div className='settings'>
+        <div className={styles.settings}>
             <h1>Settings </h1>
             <span 
             onClick={toggleSettings}
             style={{fontSize: '16px', cursor: 'pointer', textDecoration: 'underline', fontWeight: '500', marginLeft: '10vw', position: 'absolute', top: 50, right: 50}}>close</span>
-            <ul className='settings-list'>
+            <ul className={styles['settings-list']>
                 {/* <li className={state.theme==='dark' ? 'enabled': ""}>Dark mode</li> */}
                 {
                     (Object.keys(state) as any[]).map((key: keyof typeof state, idx) => {
                         if (key.startsWith("toggle")) {
                             let k = key.slice(6) as keyof EditorState;
                             return (
-                                <li key={idx} className={state[k] ? 'enabled': ""} onClick={()=>typeof state[key] === "function" && (state[key] as ((f:any)=>void))(state[k])}>toggle {k}</li>
+                                <li key={idx} className={state[k] ? styles.enabled : ""} onClick={()=>typeof state[key] === "function" && (state[key] as ((f:any)=>void))(state[k])}>toggle {k}</li>
                             )
                         }
                         return <></>
                     })
                 }
-                <li className='multiple-item'>
+                <li className={styles['multiple-item']}>
                     <button onClick={() => state.setfontSize(state.fontSize+1)}>+</button>
                     fontSize - {state.fontSize}
                     <button onClick={() => state.setfontSize(state.fontSize-1)}>-</button>
@@ -39,10 +39,10 @@ export const Settings:React.FC<{toggleSettings: ()=>void}> = ({toggleSettings}) 
                     (Object.keys(options) as any[]).map((key: keyof typeof options, idx) => {
                         if (typeof options[key] === 'object') {
                             return (
-                                <li  key={idx} className='multiple-item'>
+                                <li  key={idx} className={styles['multiple-item']}>
                                     {key} {"   "}
                                     {options[key].map((option) => {
-                                        return <span  key={idx} onClick={() => (state[("set" + key) as keyof EditorState] as any)(option)} className={state[key]===option?"enabled":""}>{option}</span>
+                                        return <span  key={idx} onClick={() => (state[("set" + key) as keyof EditorState] as any)(option)} className={state[key]===option? styles.enabled:""}>{option}</span>
                                     })}
                                 </li>
                             )
